dcaec980aed8b183370ea0ec4b6db9886c4e019c
0mniManTheCpu <mymhlu001@myuct.ac.za>
2025-06-13 22:53:44 +0200

    modified mini project 4

--- mini_project_4.py
+++ mini_project_4.py
@@ -71,3 +71,25 @@ print(b)
 # b = 13, a = 13, i = 0 , a = 13 * 13 * 0 = 0
 # a = 0, b = 0, i = 1
 #
+
+# check if a number is in the array
+def search(array, target, index = 0):
+    print(target, array, index)
+    # empty array
+    if not array:
+        return False
+
+# array have 1 element [10] target = 1
+    if len(array) == 1:
+        return array[0] == target
+
+    # [1, 2, 3] target = 2
+    # the rest of elements
+    if array[0] == target:
+        return True
+    return search(array[1:], target, index + 1)
+
+array = [10, 11, -6, 9, 5, 7, 8, 12, 90, 47, 112, 30, 20, 900, 91, -50]
+target = 112
+
+print(search(array, target))
